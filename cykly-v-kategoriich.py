c8dcf04a870967b585d1be3294790a9a601e94ff
dvorapa <dvorapa@seznam.cz>
2022-11-18 23:56:32 +0100

    Zjednoduseni a zrychleni cyklu v kategoriich

--- cykly-v-kategoriich.py
+++ cykly-v-kategoriich.py
@@ -1,48 +1,34 @@
+#!/usr/bin/env python3
 import pywikibot
 
 s = pywikibot.Site()
 
-a = set()
-v = set()
-w = []
-for i in s.allpages(namespace=14):
-    b = i.title(with_ns=False)[0]
-    if not b in a:
-        a.add(b)
-        print(b)
-    subcats = i.subcategories(recurse=True)
-    if next(subcats, False) and next(i.categories().__iter__(), False):
-        g = []
-        f = set()
-        for j in subcats:
-            if g:
-                g.append(j)
-            elif j in f:
-                break
-            if j == i:
-                v.add(i)
-                if not g:
-                    g = [i]
-                else:
-                    w.append(g)
-                    g = []
-                    print(i)
-                    break
-            f.add(j)
-            
-
-f = set()
+f = set()  # set of seen looping cats
+a = ''
 t = ''
-for g in w:
-    l = []
-    for i in g:
-        if i in v:
-            l.append(i)
-    u = frozenset(l)
-    if not u in f:
-        f.add(u)
-        t += '# ' + ' > '.join(i.title(as_link=True, textlink=True) for i in l) + '\n'
+for i in s.allpages(namespace=14):
+  # is a subcategory and contains 1 or more subcategories and hasn't been seen yet
+  if not i.isEmptyCategory() and next(i.categories(total=1), False) and not i in f:
+    b = i.title(with_ns=False)[0]
+    if b != a:  # print first letter for progress
+      a = b
+      print(b)
+    c = i.subcategories(recurse=True)
+    if i in c:  # is there a loop?
+      g = set()  # set of current looping cats
+      for j in c:
+        if g:  # add loop member
+          g.add(j)
+        if j == i:  # found beginning/end of the loop
+          if not g:  # start tracking the loop
+            g.add(i)
+          else:  # finish loop
+            t += '\n# ' + ' > '.join(k.title(as_link=True, textlink=True) for k in g)
+            f.update(g)
+            print(g)
+            g = set()
+            break
 
 p = pywikibot.Page(s, 'Wikipedie:Údržbové seznamy/Cykly v kategoriích/seznam')
-p.text += '\n' + t
+p.text += t
 p.save(summary='Robot: aktualizace')
