61dec313f34279823dc42e699c5e180c394f04b7
dvorapa <dvorapa@seznam.cz>
2022-11-19 20:11:18 +0100

    Optimalizace pameti cyklu

--- cykly-v-kategoriich.py
+++ cykly-v-kategoriich.py
@@ -5,7 +5,7 @@ s = pywikibot.Site()
 
 f = set()  # set of seen looping cats
 a = ''
-t = ''
+t = set()
 for i in s.allpages(namespace=14):
   # is a subcategory and contains 1 or more subcategories and hasn't been seen yet
   if not i.isEmptyCategory() and next(i.categories(total=1), False) and not i in f:
@@ -13,22 +13,25 @@ for i in s.allpages(namespace=14):
     if b != a:  # print first letter for progress
       a = b
       print(b)
+    del(b)
     c = i.subcategories(recurse=True)
     if i in c:  # is there a loop?
-      g = set()  # set of current looping cats
+      g = []  # list of current looping cats
+      d = g.append
       for j in c:
         if g:  # add loop member
-          g.add(j)
+          d(j)
         if j == i:  # found beginning/end of the loop
           if not g:  # start tracking the loop
-            g.add(i)
+            d(i)
           else:  # finish loop
-            t += '\n# ' + ' > '.join(k.title(as_link=True, textlink=True) for k in g)
+            t.add('\n# ' + ' > '.join(k.title(as_link=True, textlink=True) for k in g))
             f.update(g)
             print(g)
-            g = set()
+            del(d)
+            del(g)
             break
 
 p = pywikibot.Page(s, 'Wikipedie:Údržbové seznamy/Cykly v kategoriích/seznam')
-p.text += t
+p.text += ''.join(t)
 p.save(summary='Robot: aktualizace')
