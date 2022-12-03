bcd2191548ccb54b1767ccc06bf1b03b4bbba779
tools.dvorapabot <tools.dvorapabot@tools-sgebastion-10.tools.eqiad1.wikimedia.cloud>
2022-12-03 22:35:56 +0000

    Revert cyklu v kategoriich na puvodni stav

--- cykly-v-kategoriich.py
+++ cykly-v-kategoriich.py
@@ -1,37 +1,48 @@
-#!/usr/bin/env python3
 import pywikibot
 
 s = pywikibot.Site()
 
-f = set()  # set of seen looping cats
-a = ''
-t = set()
+a = set()
+v = set()
+w = []
 for i in s.allpages(namespace=14):
-  # is a subcategory and contains 1 or more subcategories and hasn't been seen yet
-  if not i.isEmptyCategory() and next(i.categories(total=1), False) and not i in f:
     b = i.title(with_ns=False)[0]
-    if b != a:  # print first letter for progress
-      a = b
-      print(b, flush=True)
-    del(b)
-    c = i.subcategories(recurse=True)
-    if i in c:  # is there a loop?
-      g = []  # list of current looping cats
-      d = g.append
-      for j in c:
-        if g:  # add loop member
-          d(j)
-        if j == i:  # found beginning/end of the loop
-          if not g:  # start tracking the loop
-            d(i)
-          else:  # finish loop
-            t.add('\n# ' + ' > '.join(k.title(as_link=True, textlink=True) for k in g))
-            f.update(g)
-            print(g, flush=True)
-            del(d)
-            del(g)
-            break
+    if not b in a:
+        a.add(b)
+        print(b)
+    subcats = i.subcategories(recurse=True)
+    if next(subcats, False) and next(i.categories().__iter__(), False):
+        g = []
+        f = set()
+        for j in subcats:
+            if g:
+                g.append(j)
+            elif j in f:
+                break
+            if j == i:
+                v.add(i)
+                if not g:
+                    g = [i]
+                else:
+                    w.append(g)
+                    g = []
+                    print(i)
+                    break
+            f.add(j)
+
+
+f = set()
+t = ''
+for g in w:
+    l = []
+    for i in g:
+        if i in v:
+            l.append(i)
+    u = frozenset(l)
+    if not u in f:
+        f.add(u)
+        t += '# ' + ' > '.join(i.title(as_link=True, textlink=True) for i in l) + '\n'
 
 p = pywikibot.Page(s, 'Wikipedie:Údržbové seznamy/Cykly v kategoriích/seznam')
-p.text += ''.join(t)
+p.text += '\n' + t
 p.save(summary='Robot: aktualizace')
